c716180873b3918fea5b493493f94e1a37fa7109
Nguyen Thanh Danh <danh49003@gmail.com>
2026-02-07 15:55:45 +0700

    changeSupabase

---
+++ IdeaCollectionSystem.ApplicationCore/DTO/CategoryDto.cs
@@ -0,0 +1,12 @@
+﻿using IdeaCollectionIdea.Common.Constants;
+using System.ComponentModel.DataAnnotations;
+namespace IdeaCollectionSystem.Models
+{
+    public class CategoryDto
+    {
+        public Guid? Id { get; set; }
+        [Required]
+        [MaxLength(MaxLengths.NAME)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
