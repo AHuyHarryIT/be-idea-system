1436825ecf43b1b4212c7e67608b89fc3a436fd1
DHCuong27 <dangcuong9331@gmail.com>
2026-03-07 01:49:05 +0700

    delete dto

--- IdeaCollectionSystem.Service.Models.DTOs/IdeaCreateDto.cs
+++
@@ -1,9 +0,0 @@
-public class IdeaCreateDto
-{
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public Guid CategoryId { get; set; }
-    public bool IsAnonymous { get; set; }
-    public List<string>? FilePaths { get; set; }
-    public int DepartmentId { get; set; } 
-}
