62eca393bbe957b0fc7b2c0301aaa5ad73ef7da7
DHCuong27 <dangcuong9331@gmail.com>
2026-03-06 22:31:30 +0700

    AddMigrationFixIdeaReaction

--- IdeaCollectionSystem.Datalayer/IdeaCollectionDbContext.cs
+++ IdeaCollectionSystem.Datalayer/IdeaCollectionDbContext.cs
@@ -19,6 +19,7 @@ namespace IdeaCollectionSystem.Datalayer
 		public DbSet<Role> Roles { get; set; }
 		public DbSet<Submission> Submissions { get; set; }
 		public DbSet<Idea> Ideas { get; set; }
+		public DbSet<IdeaReactions> IdeaReactions { get; set; }
 		public DbSet<EmailOutBox> EmailOutBoxes { get; set; }
 		#endregion
 
@@ -27,6 +28,7 @@ namespace IdeaCollectionSystem.Datalayer
 			base.OnModelCreating(modelBuilder);
 
 			#region Master Entities: Role, Department, Category, Submission
+			
 			modelBuilder.Entity<Role>(entity =>
 			{
 				entity.ToTable("Roles");
@@ -54,12 +56,13 @@ namespace IdeaCollectionSystem.Datalayer
 				entity.ToTable("Submissions");
 				entity.HasKey(x => x.Id);
 				entity.Property(x => x.Name).HasMaxLength(MaxLengths.NAME).IsRequired();
+			
 				entity.Property(x => x.ClousureDate).IsRequired();
 				entity.Property(x => x.FinalClousureDate).IsRequired();
 			});
 			#endregion
 
-			#region User (Master/Security Entity)
+			#region User (Security Entity)
 			modelBuilder.Entity<User>(entity =>
 			{
 				entity.ToTable("Users");
@@ -67,13 +70,13 @@ namespace IdeaCollectionSystem.Datalayer
 				entity.Property(x => x.UserName).HasMaxLength(MaxLengths.NAME).IsRequired();
 				entity.Property(x => x.HashPassword).HasMaxLength(MaxLengths.HASH_PASSWORD).IsRequired();
 
-				
+			
 				entity.HasOne(x => x.Role)
 					.WithMany(x => x.Users)
 					.HasForeignKey(x => x.RoleId)
 					.OnDelete(DeleteBehavior.Restrict);
 
-				
+			
 				entity.HasOne(x => x.Department)
 					.WithMany(x => x.Users)
 					.HasForeignKey(x => x.DepartmentId)
@@ -88,23 +91,25 @@ namespace IdeaCollectionSystem.Datalayer
 				entity.HasKey(x => x.Id);
 				entity.Property(x => x.Text).HasMaxLength(MaxLengths.TEXT).IsRequired();
 
-		
+				
 				entity.HasOne(x => x.User)
 					.WithMany(x => x.Ideas)
 					.HasForeignKey(x => x.UserId)
 					.OnDelete(DeleteBehavior.Restrict);
 
+				
 				entity.HasOne(x => x.Submission)
 					.WithMany(x => x.Ideas)
 					.HasForeignKey(x => x.SubmissionId)
 					.OnDelete(DeleteBehavior.Restrict);
 
+				
 				entity.HasOne(x => x.Category)
 					.WithMany(x => x.Ideas)
 					.HasForeignKey(x => x.CategoryId)
 					.OnDelete(DeleteBehavior.Restrict);
 
-			
+				
 				entity.HasOne(x => x.Department)
 					.WithMany(x => x.Ideas)
 					.HasForeignKey(x => x.DepartmentId)
@@ -112,7 +117,31 @@ namespace IdeaCollectionSystem.Datalayer
 			});
 			#endregion
 
-			#region Comment & EmailOutBox
+			#region IdeaReactions (Fixed Logic)
+			modelBuilder.Entity<IdeaReactions>(entity =>
+			{
+				entity.ToTable("IdeaReactions");
+
+				// Khóa chính phức hợp
+				entity.HasKey(x => new { x.UserId, x.IdeaId });
+
+				// Quan hệ với Idea
+				entity.HasOne(x => x.Idea)
+					.WithMany(x => x.IdeaReactions)
+					.HasForeignKey(x => x.IdeaId)
+					.OnDelete(DeleteBehavior.Cascade);
+
+				entity.HasOne(x => x.User)
+					.WithMany() 
+					.HasForeignKey(x => x.UserId)
+					.OnDelete(DeleteBehavior.Restrict);
+
+				entity.Property(x => x.Reaction).IsRequired();
+			});
+			#endregion
+
+
+			#region Comment & Email Log
 			modelBuilder.Entity<Comment>(entity =>
 			{
 				entity.ToTable("Comments");
@@ -132,6 +161,7 @@ namespace IdeaCollectionSystem.Datalayer
 			modelBuilder.Entity<EmailOutBox>(entity =>
 			{
 				entity.ToTable("EmailOutBoxes");
+			
 				entity.HasOne(e => e.Comment)
 					.WithMany()
 					.HasForeignKey(e => e.CommentId)
---
+++ IdeaCollectionSystem.Datalayer/Migrations/IdeaCollectionDb/20260306150537_AddIdeaReactions.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdeaCollectionSystem.Datalayer.Migrations.IdeaCollectionDb
+{
+    /// <inheritdoc />
+    public partial class AddIdeaReactions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "IdeaReactionsIdeaId",
+                table: "Users",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "IdeaReactionsUserId",
+                table: "Users",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "IdeaReactionsIdeaId",
+                table: "Ideas",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "IdeaReactionsUserId",
+                table: "Ideas",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "IdeaReactions",
+                columns: table => new
+                {
+                    IdeaId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Reaction = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IdeaReactions", x => new { x.UserId, x.IdeaId });
+                    table.ForeignKey(
+                        name: "FK_IdeaReactions_Ideas_IdeaId",
+                        column: x => x.IdeaId,
+                        principalTable: "Ideas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IdeaReactions_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_IdeaReactionsUserId_IdeaReactionsIdeaId",
+                table: "Users",
+                columns: new[] { "IdeaReactionsUserId", "IdeaReactionsIdeaId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ideas_IdeaReactionsUserId_IdeaReactionsIdeaId",
+                table: "Ideas",
+                columns: new[] { "IdeaReactionsUserId", "IdeaReactionsIdeaId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdeaReactions_IdeaId",
+                table: "IdeaReactions",
+                column: "IdeaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ideas_IdeaReactions_IdeaReactionsUserId_IdeaReactionsIdeaId",
+                table: "Ideas",
+                columns: new[] { "IdeaReactionsUserId", "IdeaReactionsIdeaId" },
+                principalTable: "IdeaReactions",
+                principalColumns: new[] { "UserId", "IdeaId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_IdeaReactions_IdeaReactionsUserId_IdeaReactionsIdeaId",
+                table: "Users",
+                columns: new[] { "IdeaReactionsUserId", "IdeaReactionsIdeaId" },
+                principalTable: "IdeaReactions",
+                principalColumns: new[] { "UserId", "IdeaId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ideas_IdeaReactions_IdeaReactionsUserId_IdeaReactionsIdeaId",
+                table: "Ideas");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_IdeaReactions_IdeaReactionsUserId_IdeaReactionsIdeaId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "IdeaReactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_IdeaReactionsUserId_IdeaReactionsIdeaId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Ideas_IdeaReactionsUserId_IdeaReactionsIdeaId",
+                table: "Ideas");
+
+            migrationBuilder.DropColumn(
+                name: "IdeaReactionsIdeaId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IdeaReactionsUserId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IdeaReactionsIdeaId",
+                table: "Ideas");
+
+            migrationBuilder.DropColumn(
+                name: "IdeaReactionsUserId",
+                table: "Ideas");
+        }
+    }
+}
